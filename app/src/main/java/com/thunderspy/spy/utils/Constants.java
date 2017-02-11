6c6e99c65ad37ff34d102a168da6f1ed064ed968
Ariyan Khan <ariyankhan46@gmail.com>
2017-02-11 22:06:12 +0530

    Update

---
+++ app/src/main/java/com/thunderspy/spy/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.thunderspy.spy.utils;
+
+/**
+ * Created by ariyan on 2/11/17.
+ */
+
+public final class Constants {
+}
---
+++ app/src/main/java/com/thunderspy/spy/utils/Utils.java
@@ -0,0 +1,9 @@
+package com.thunderspy.spy.utils;
+
+/**
+ * Created by ariyan on 2/11/17.
+ */
+
+public final class Utils {
+
+}
